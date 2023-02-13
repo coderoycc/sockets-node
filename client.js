5198fbc1551535676cd5ae399702e13c2b414685
Roberto Chambi <rcchambi4@umsa.bo>
2023-02-12 23:17:39 -0400

    Chat server-cliente

--- client.js
+++ client.js
@@ -4,13 +4,19 @@ const readline = require('readline').createInterface({
   output: process.stdout
 }) //Lectura desde consola
 const socket = new Socket()
+const END = 'end' // Palabra para terminar la conexión
 
 socket.connect({host:'localhost', port:3000})
 socket.setEncoding('utf-8') 
 
 // Evento por si se encuentra una linea en la consola
-readline.on('line', (line) => {
-  socket.write(line) // Mandamos la linea que se escribió
+readline.on('line', (message) => {
+  if(message === END){ //cerramos socket y terminamos proceso
+    console.log('Conexión terminada');
+    socket.end() // esperamos a que el servidor confirme cierre 
+  }else{
+    socket.write(message) // Mandamos la linea que se escribió
+  }
 })
 
 
@@ -18,3 +24,6 @@ readline.on('line', (line) => {
 socket.on('data', (data) => {
   console.log(data)
 })
+
+// Verificamos si el servidor confirmo cierre
+socket.on('close', () => process.exit(0))
--- server.js
+++ server.js
@@ -3,14 +3,26 @@ const { Server } = require('net')
 // Instanciamos servidor
 const server = new Server()
 
+const END = 'end' //palabra para terminar la conexión
+
 // Preparamos para cualquier conexión
 server.on('connection', (socket) => {
-  console.log('Se conectó un nuevo usuario con IP: ', socket.remoteAddress,':', socket.remotePort)
+  const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`
+  console.log('Nueva conexión:', remoteSocket)
   
   socket.setEncoding('utf-8') // Codificación de datos
   // Revisamos si el socket tiene datos (evento)
   socket.on('data', (data) => {
-    socket.write(data.toUpperCase()) // Devolvemos un mensaje (con mayusculas)
+    if(data === END){
+      socket.end()
+    }else{
+      console.log(`${remoteSocket}->${data}`)
+    }
+  })
+
+  // Verificamos si ambos sockets se cerraron
+  socket.on('close', ()=>{
+    console.log('Conexión con ',remoteSocket,'terminada')
   })
 })
 
