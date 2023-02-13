4b137d7a7433c1508c0f93ef49d7e0650a17e7ac
Roberto Chambi <rcchambi4@umsa.bo>
2023-02-12 22:54:31 -0400

    Envio y recepción de datos con client

--- client.js
+++ client.js
@@ -6,7 +6,13 @@ const readline = require('readline').createInterface({
 const socket = new Socket()
 
 socket.connect({host:'localhost', port:3000})
-socket.setEncoding('utf-8')
+socket.setEncoding('utf-8') 
+
+// Evento por si se encuentra una linea en la consola
+readline.on('line', (line) => {
+  socket.write(line) // Mandamos la linea que se escribió
+})
+
 
 // Verificamos si el servidor nos envió datos
 socket.on('data', (data) => {
