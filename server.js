809ae7099a59d20a2f31211c27dba4200acb6d86
Roberto Chambi <rcchambi4@umsa.bo>
2023-02-13 21:12:59 -0400

    Modularizando server

--- server.js
+++ server.js
@@ -1,32 +1,54 @@
 const { Server } = require('net')
 
-// Instanciamos servidor
-const server = new Server()
-
+const host = '0.0.0.0'
 const END = 'end' //palabra para terminar la conexión
-
-// Preparamos para cualquier conexión
-server.on('connection', (socket) => {
-  const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`
-  console.log('Nueva conexión:', remoteSocket)
+const error = (err) => {
+  console.error(err)
+  process.exit(1) // NO OK
+}
+const listen = (port) => {
+  // Instanciamos servidor
+  const server = new Server()
+  // Preparamos para cualquier conexión
+  server.on('connection', (socket) => {
+    const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`
+    console.log('Nueva conexión:', remoteSocket)
+    
+    socket.setEncoding('utf-8') // Codificación de datos
+    // Revisamos si el socket tiene datos (evento)
+    socket.on('data', (data) => {
+      if(data === END){
+        socket.end()
+      }else{
+        console.log(`${remoteSocket}->${data}`)
+      }
+    })
   
-  socket.setEncoding('utf-8') // Codificación de datos
-  // Revisamos si el socket tiene datos (evento)
-  socket.on('data', (data) => {
-    if(data === END){
-      socket.end()
-    }else{
-      console.log(`${remoteSocket}->${data}`)
-    }
+    // Verificamos si ambos sockets se cerraron
+    socket.on('close', ()=>{
+      console.log('Conexión con ',remoteSocket,'terminada')
+    })
   })
+  server.listen({port:port, host:'0.0.0.0'}, () => {
+    console.log("Listening on port ", port)
+ })
+}
 
-  // Verificamos si ambos sockets se cerraron
-  socket.on('close', ()=>{
-    console.log('Conexión con ',remoteSocket,'terminada')
-  })
-})
+const main = ()=>{
+  if(process.argv.length !== 3){
+    error(`Usé node ${__filename} port`)
+  }
+  console.log(process.argv) // Argumentos de la consola
+  let port = process.argv[2]
+  if(isNaN(port)){
+    error(`Puerto Invalido ${port}`)
+  }
+  port = Number(port)
+  listen(port)
+}
+
+if(require.main === module){
+  main()
+}// Verificamos que se esté ejecutando este programa y no se esté importando
 
 
-server.listen({port:3000, host:'0.0.0.0'}, () => {
-  console.log("Listening on port 3000")
-})
